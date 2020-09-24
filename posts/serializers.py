bffc392e3ea0560fd46d02e7dfce5a7b0c1140d4
ina <bbangina@gmail.com>
2020-09-24 14:08:56 +0900

    인아커밋 post 리스트뷰 추가

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,24 +3,22 @@ from .models import Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-    # 컬럼 읽기전용으로 만들기
+    # poster = username
     poster = serializers.ReadOnlyField(source="user.username")
-    poster_id = serializers.ReadOnlyField(source="user.id")
-    views = serializers.ReadOnlyField(source="views")
-    # field에 메소드 추가(votes는 원래 model에 없는 필드)
-    comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'poster', 'poster_id', 'category', 'title', 'content', 'img_path',
-                  'created', 'views']
-
-    def get_comments(self, post):
-        # 함수형태로 해당 post의 코멘트를 가져옴
-        return Comment.objects.filter(post=post)
+        fields = [
+            'category',
+            'title',
+            'content',
+            'img_path',
+            'created',
+            'views',
+        ]
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Vote
+        model = Comment
         fields = "__all__"
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostList(generics.ListCreateAPIView):  # 뷰 기반의 클래스 생성
+    queryset = Post.objects.all()  # DB에서 쿼리셋 전부 가져왕
+    serializer_class = PostSerializer
+    # IsAuthenticated:로그인유저만 /IsAuthenticatedOrReadOnly: 로그인유저는 post가능,아니면 readonly
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # 예약어임
+    def perform_create(self, serializer):
+        # 시리얼라이저야, 저장할 때 poster컬럼은 POST요청자 이름을 넣어
+        serializer.save(poster=self.request.user)
+
+
+# class PostDetail(DetailView):
